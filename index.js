95f3b2f5527c92207dc3a65fb812008bb46157e3
apinya2020 <apinya.jitsampita@gmail.com>
2019-05-07 11:12:39 +0700

    app-demo

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+function print() {
+    const name = 'Nuttavut Thongjor'
+    
+    // return function ออกไปเมื่อเรียกฟังก์ชัน print
+    return function() {
+      console.log(name)
+    }
+  }
+  
+  const printName = print()
+  printName() // Nuttavut Thongjor
